29b9530c7dc54830927005cea470101fb2d92b2c
Adam Walker <adam.walker@nicta.com.au>
2014-03-17 16:18:20 -0400

    DdNode -> DDNode conversion

--- CuddConvert.hs
+++ CuddConvert.hs
@@ -4,9 +4,17 @@ import System.IO.Unsafe
 import Foreign.ForeignPtr
 
 import CuddInternal
+import CuddC
 
 toDdNode :: DdManager -> DDNode s u -> DdNode
 toDdNode (DdManager m) (DDNode d) = DdNode $ unsafePerformIO $ newForeignPtrEnv deref m d
 
 toDdManager :: STDdManager s u -> DdManager
 toDdManager = DdManager . unSTDdManager
+
+toDDNode :: DdNode -> DDNode s u 
+toDDNode (DdNode fp) = unsafePerformIO $ do
+    let p = unsafeForeignPtrToPtr fp
+    cuddRef p
+    return $ DDNode p
+
