3a2b2308ad832e7f44e002f0bf2c12ad7a39b62a
Adam Walker <adamwalker10@gmail.com>
2014-12-29 19:23:55 +1300

    sane names in Cudd.Convert

--- Cudd/Convert.hs
+++ Cudd/Convert.hs
@@ -1,8 +1,8 @@
 module Cudd.Convert (
-    toDdNode,
-    toDdManager,
-    toDDNode,
-    getSTManager
+    fromImperativeNode,
+    fromImperativeManager,
+    toImperativeNode,
+    toImperativeManager
     ) where
 
 import System.IO.Unsafe
@@ -14,19 +14,19 @@ import Control.Monad.ST.Unsafe
 import Cudd.Internal
 import Cudd.C
 
-toDdNode :: DdManager -> DDNode s u -> DdNode
-toDdNode (DdManager m) (DDNode d) = DdNode $ unsafePerformIO $ do 
+fromImperativeNode :: DdManager -> DDNode s u -> DdNode
+fromImperativeNode (DdManager m) (DDNode d) = DdNode $ unsafePerformIO $ do 
     cuddRef d
     newForeignPtrEnv deref m d
 
-toDdManager :: STDdManager s u -> DdManager
-toDdManager = DdManager . unSTDdManager
+fromImperativeManager :: STDdManager s u -> DdManager
+fromImperativeManager = DdManager . unSTDdManager
 
-toDDNode :: DdNode -> ST s (DDNode s u)
-toDDNode (DdNode fp) = unsafeIOToST $ do
+toImperativeNode :: DdNode -> ST s (DDNode s u)
+toImperativeNode (DdNode fp) = unsafeIOToST $ do
     let p = unsafeForeignPtrToPtr fp
     cuddRef p
     return $ DDNode p
 
-getSTManager :: DdManager -> STDdManager s u
-getSTManager (DdManager m) = STDdManager m
+toImperativeManager :: DdManager -> STDdManager s u
+toImperativeManager (DdManager m) = STDdManager m
